0d97c12fca0517ef73d60dce2b97bcd997b79092
Ziad Ahmed <ziiadd2211@gmail.com>
2023-09-05 01:37:25 +0300

    edit pro name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/DollabyReactApp",
+  base: "/Ecommerce-React-App",
   plugins: [react()],
 });
